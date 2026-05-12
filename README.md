025efcfa237c69448642c8dfcd1ba5132d80a3b3
AdrianP2424 <guiacuencaapp@gmail.com>
2026-05-11 20:07:04 -0500

    RECURSIVIDAD

---
+++ README.md
@@ -0,0 +1,11 @@
+### RECURSIVIDAD 
+
+## ESTUDIANTE:
+ - ADRIAN PLAZA
+## PC 1.3 Ejercicios 1 a 3
+### DESCRIPCION:
+
+Descripcion de los ejercicios realizados.
+
+
+
---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static  void main(String[] args) throws Exception {
+        Ejercicios ejercicios = new Ejercicios();
+        /* 
+        System.out.println("Ejercicio 1");
+        
+        int rest1 = ejercicios.sumaConcecuitivos(5);{
+            System.out.println(rest1 );
+        
+        int  rest2= ejercicios.sumaConcecuitivosPasos(5);{
+            System.out.println(rest2);
+        }
+         */
+        System.out.println("Ejercicio 2");
+        int  rest3= ejercicios.getPotencia(2,5);{
+            System.out.println(rest3);
+        }
+
+
+    }
+
+}
